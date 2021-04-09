c5e391274786f7836d91c425909d9eeed956bbb1
saad <saad-mogren@hotmail.com>
2021-04-09 03:05:44 +0300

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,7 +5,7 @@ from app import db
 
 class Task(db.Model):
     __tablename__ = "Task"
-    id = db.Column(db.String(), primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(), nullable=False)
     duration = db.Column(db.Integer, nullable=False)
     start = db.Column(db.DateTime, nullable=False)
@@ -42,27 +42,19 @@ class Task(db.Model):
 
 class Resource(db.Model):
     __tablename__ = "Resource"
-    id = db.Column(db.String(), primary_key=True, nullable=False)
-    name = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), primary_key=True, nullable=False)
     type = db.Column(db.String(), nullable=False)
-    material = db.Column(db.String(), nullable=False)
     max = db.Column(db.Float, nullable=False)
-    rate = db.Column(db.String(), nullable=False)
-    ovt = db.Column(db.String(), nullable=False)
-    cost = db.Column(db.Float, nullable=False)
+    rate = db.Column(db.Float, nullable=False)
     tasks = db.relationship('Tasks_Resources', backref='resource', lazy=True, cascade='all, delete')
 
 
 
-    def __init__(self, id, name, type, material, max, rate, ovt, cost):
-        self.id  = id
+    def __init__(self, name, type, max, rate):
         self.name = name
         self.type = type
-        self.material = material
         self.max = max
         self.rate = rate
-        self.ovt = ovt
-        self.cost = cost
 
 
     def insert(self):
@@ -78,17 +70,38 @@ class Resource(db.Model):
 
     def format(self):
         return {
-            'id': self.id,
             'name': self.name,
             'type': self.type,
             'max': self.max,
             'rate': self.rate,
-            'ovt': self.ovt,
-            'cost': self.cost,
-
         }
 class Tasks_Resources(db.Model):
     __tablename__ = "Tasks_Resources"
     task_id = db.Column(db.Integer(), db.ForeignKey("Task.id", ondelete="CASCADE"), primary_key=True)
-    resource_id = db.Column(db.Integer(), db.ForeignKey("Resource.id", ondelete="CASCADE"), primary_key=True)
+    resource_name = db.Column(db.String(), db.ForeignKey("Resource.name", ondelete="CASCADE"), primary_key=True)
     total_cost = db.Column(db.Float, nullable=True)
+
+
+    def __init__(self, task_id, resource_name, total_cost):
+        self.task_id = task_id
+        self.resource_name = resource_name
+        self.total_cost = total_cost
+
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'task_id': self.task_id,
+            'resource_name': self.resource_name,
+            'total_cost': self.total_cost
+        }
