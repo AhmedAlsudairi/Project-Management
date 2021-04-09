08e9ca18a84a70682a09c7814339ed317ba43932
saad <saad-mogren@hotmail.com>
2021-04-09 03:05:34 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:admin@localhost:5432/rt-tool'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:admin@localhost:5432/pm'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
