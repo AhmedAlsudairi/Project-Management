37143b68f04570ed127ff3cf2ba87f2d81eb9d1b
saad <saad-mogren@hotmail.com>
2021-04-09 04:16:48 +0300

    update models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,11 +1,15 @@
 from sqlalchemy import Column, String, create_engine
 from app import db
+from datetime import datetime
+import shortuuid
 
-
+def generateID(name):
+    random_string = shortuuid.ShortUUID().random(length=5)
+    return str(name)+'_'+random_string
 
 class Task(db.Model):
     __tablename__ = "Task"
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.String(), primary_key=True, nullable=False)
     name = db.Column(db.String(), nullable=False)
     duration = db.Column(db.Integer, nullable=False)
     start = db.Column(db.DateTime, nullable=False)
@@ -13,12 +17,12 @@ class Task(db.Model):
     resources = db.relationship('Tasks_Resources', backref='task', lazy=True, cascade='all, delete')
 
 
-    def __init__(self, id, name, duration, start, finish):
-        self.id  = id
+    def __init__(self, name, duration, start, finish):
+        self.id  = generateID(name)
         self.name = name
         self.duration = duration
-        self.start = start
-        self.finish = finish
+        self.start = datetime.strptime(start, '%d/%m/%Y')
+        self.finish = datetime.strptime(finish, '%d/%m/%Y')
 
     def insert(self):
         db.session.add(self)
@@ -39,10 +43,20 @@ class Task(db.Model):
             'start': self.start,
             'finish': self.finish
         }
+    def format_report(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'duration': self.duration,
+            'start': self.start,
+            'finish': self.finish,
+            'resources': [Resource.query.get(resource.resource_id).name for resource in self.resources]
+        }
 
 class Resource(db.Model):
     __tablename__ = "Resource"
-    name = db.Column(db.String(), primary_key=True, nullable=False)
+    id = db.Column(db.String(), primary_key=True, nullable=False)
+    name =  db.Column(db.String(), nullable=False)
     type = db.Column(db.String(), nullable=False)
     max = db.Column(db.Float, nullable=False)
     rate = db.Column(db.Float, nullable=False)
@@ -51,6 +65,7 @@ class Resource(db.Model):
 
 
     def __init__(self, name, type, max, rate):
+        self.id  = generateID(name)
         self.name = name
         self.type = type
         self.max = max
@@ -77,14 +92,14 @@ class Resource(db.Model):
         }
 class Tasks_Resources(db.Model):
     __tablename__ = "Tasks_Resources"
-    task_id = db.Column(db.Integer(), db.ForeignKey("Task.id", ondelete="CASCADE"), primary_key=True)
-    resource_name = db.Column(db.String(), db.ForeignKey("Resource.name", ondelete="CASCADE"), primary_key=True)
+    task_id = db.Column(db.String(), db.ForeignKey("Task.id", ondelete="CASCADE"), primary_key=True)
+    resource_id = db.Column(db.String(), db.ForeignKey("Resource.id", ondelete="CASCADE"), primary_key=True)
     total_cost = db.Column(db.Float, nullable=True)
 
 
-    def __init__(self, task_id, resource_name, total_cost):
+    def __init__(self, task_id, resource_id, total_cost):
         self.task_id = task_id
-        self.resource_name = resource_name
+        self.resource_id = resource_id
         self.total_cost = total_cost
 
 
@@ -102,6 +117,6 @@ class Tasks_Resources(db.Model):
     def format(self):
         return {
             'task_id': self.task_id,
-            'resource_name': self.resource_name,
+            'resource_name': self.resource_id,
             'total_cost': self.total_cost
         }
